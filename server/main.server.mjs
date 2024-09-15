08aa3cfecb0de779b56d7109b5135ff126025957
Goutamsingla01 <122777044+Goutamsingla01@users.noreply.github.com>
2024-09-15 17:08:19 +0530

    deploy angular app to github

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-AF2J3CZ5.mjs";import"./chunk-VLLAXQQU.mjs";import"./chunk-5IWEE6E2.mjs";import"./chunk-43A2YVCL.mjs";import"./chunk-GTSSCL6B.mjs";import"./chunk-DNMIM6XO.mjs";import"./chunk-J2PXKSVZ.mjs";import"./chunk-BPXMDIMD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
