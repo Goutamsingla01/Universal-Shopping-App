0eac31f8e2d81caf3924900a8ec673cc97e231f6
Goutamsingla01 <122777044+Goutamsingla01@users.noreply.github.com>
2024-10-19 21:28:06 +0530

    database change23456789

--- backend/universal-shopping-app/src/main/java/com/shoppingapp/universal_shopping_app/UniversalShoppingAppApplication.java
+++ backend/universal-shopping-app/src/main/java/com/shoppingapp/universal_shopping_app/UniversalShoppingAppApplication.java
@@ -2,26 +2,16 @@ package com.shoppingapp.universal_shopping_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
-import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class UniversalShoppingAppApplication {
 
-	private final Environment environment;
+
 
 	// Constructor injection to get the Environment object
-	public UniversalShoppingAppApplication(Environment environment) {
-		this.environment = environment;
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(UniversalShoppingAppApplication.class, args);
 	}
-	@PostConstruct
-	public void init() {
-		// Print active profiles
-		System.out.println("Active Profiles: " + String.join(", ", environment.getActiveProfiles()));
-		// Print database URL (make sure to use the right property key)
-		System.out.println("Database URL: " + environment.getProperty("spring.datasource.url"));
-	}
+
 }
